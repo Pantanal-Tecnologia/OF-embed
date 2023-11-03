366c18948710f53298a241b3e86ee4ed8cfdd7ad
Araujo Marcos <marcosaraujo.tele@gmail.com>
2023-11-03 15:28:49 -0400

    adicionado layout na rota

--- index.js
+++ index.js
@@ -89,14 +89,14 @@ class OfEmbed {
       }
   }, false);
   }
-  getTransacoesDocStatus(transacoes, callback = el => true) {
+  getTransacoesDocStatus(layout,transacoes, callback = el => true) {
     return new Promise((resolve, reject) => {
       const header = new Headers();
       header.append('Content-Type', 'application/json;charset=UTF-8')
       header.append('Access-Control-Allow-Origin', '*')
       header.append('x-access-token', this.token)
       let data = {
-        layout: this.layout,
+        layout: layout,
         sistema: this.sistema,
         transacoes: transacoes
       }
