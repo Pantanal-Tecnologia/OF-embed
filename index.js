0d3f6995737f61e1a8c3878f9a75c40f291513d6
José Vitor Barros Amorim <josevitor_amorim@hotmail.com>
2024-03-08 09:20:17 -0400

    feat: adicionando a validação doc lote

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ class OfEmbed {
   endPointBack = 'https://of.pantanaltec.com.br'
   endPointFront = 'https://of.pantanaltec.com.br'
 
-  setSistema = (sistema) => this.sistema = sistema
-  setToken = (token) => this.token = token
+  setSistema = (sistema) => (this.sistema = sistema)
+  setToken = (token) => (this.token = token)
   setDevMode = (devMode) => {
     this.devMode = devMode
-    if(this.devMode) {
+    if (this.devMode) {
       this.endPointBack = 'http://localhost:3000'
       this.endPointFront = 'http://localhost:8080'
     } else {
@@ -24,71 +24,76 @@ class OfEmbed {
   }
 
   addModal(src, target) {
-
     // Criar o iframe
-    const iframe = document.createElement('iframe');
-    iframe.classList.add('modal-iframe');
+    const iframe = document.createElement('iframe')
+    iframe.classList.add('modal-iframe')
     const url = this.endPointFront + src
     console.log(url)
     iframe.src = url
 
     if (!target) {
       // Criar o fundo escuro do modal
-      const modalBackground = document.createElement('div');
-      modalBackground.classList.add('modal-background');
-      
+      const modalBackground = document.createElement('div')
+      modalBackground.classList.add('modal-background')
+
       // Criar o conteúdo do modal
-      const modalContent = document.createElement('div');
-      modalContent.classList.add('modal-content');
-      
-      
+      const modalContent = document.createElement('div')
+      modalContent.classList.add('modal-content')
+
       // Adicionar o iframe ao conteúdo do modal
-      modalContent.appendChild(iframe);
-  
+      modalContent.appendChild(iframe)
+
       // Adicionar o conteúdo do modal ao fundo escuro
-      modalBackground.appendChild(modalContent);
-  
+      modalBackground.appendChild(modalContent)
+
       // Adicionar ao body
-      document.body.appendChild(modalBackground);
-  
+      document.body.appendChild(modalBackground)
+
       // Exibir o modal ao clicar no fundo escuro (isso é apenas um exemplo, pode ser controlado de outras maneiras)
-      modalBackground.addEventListener('click', () => this.closeModal(modalBackground));
-  
+      modalBackground.addEventListener('click', () =>
+        this.closeModal(modalBackground)
+      )
+
       // Exibir o modal (isso é apenas um exemplo, pode ser feito com base em algum evento específico)
-      modalBackground.style.display = 'block';
+      modalBackground.style.display = 'block'
       return modalBackground
     } else {
-      target?.appendChild(iframe);
+      target?.appendChild(iframe)
     }
   }
-  addWindow (src) {
+  addWindow(src) {
     const url = this.endPointFront + src
     const width = window.innerWidth * 0.8
     const height = window.innerHeight * 0.8
     // window.open(url, 'ofNfValidation', `width=${width},height=${height},location=yes`);
-    window.open(url, 'ofNfValidation');
+    window.open(url, 'ofNfValidation')
   }
-  closeModal (modal) {
-    modal.style.display = 'none'; // Esconde o modal
+  closeModal(modal) {
+    modal.style.display = 'none' // Esconde o modal
     setTimeout(() => {
-      document.body.removeChild(modal);
+      document.body.removeChild(modal)
       modal = undefined
-    }, 1000);
+    }, 1000)
     try {
       this.callback()
-    } catch (error) {
-    }
+    } catch (error) {}
   }
-  validacaoFornecedor(layout, transacoes, callback = el => true, DocAdd = [], options = null) {
+  validacaoFornecedor(
+    layout,
+    transacoes,
+    callback = (el) => true,
+    DocAdd = [],
+    options = null
+  ) {
     console.log(options)
     let params = {
       modal: true,
-      target: false
+      target: false,
     }
-    if (typeof options === "object") {
+    if (typeof options === 'object') {
       params = {
         ...params,
-        ...options
+        ...options,
       }
     }
     console.log(params)
@@ -96,63 +101,142 @@ class OfEmbed {
     this.callback = callback
     const transacoesJoined = transacoes.join(',')
     const docAddJson = JSON.stringify(DocAdd)
-    const src = `/#/API/${this.sistema}/validacaoFornecedor/${layout}/${transacoesJoined}/${this.token}/${btoa(docAddJson)}`
+    const src = `/#/API/${
+      this.sistema
+    }/validacaoFornecedor/${layout}/${transacoesJoined}/${this.token}/${btoa(
+      docAddJson
+    )}`
     let modal = null
     if (params.modal) {
       modal = this.addModal(src, params.target)
     } else {
       this.addWindow(src)
     }
-    window.addEventListener("message", (event) => {
-      console.log('message FROM', event.origin, this.endPointFront, this.endPointFront == event.origin)
-      // Verificar a origem da mensagem por razões de segurança
-      if (event.origin !== this.endPointFront) {
-          return; // Não é a origem esperada, não faça nada
-      }
-      // Processar a mensagem
-      console.log("Mensagem recebida:", event.data, event.data == 'OK');
-      if (event.data == 'OK') {
-        if (modal) {
-          this.closeModal(modal)
+    window.addEventListener(
+      'message',
+      (event) => {
+        console.log(
+          'message FROM',
+          event.origin,
+          this.endPointFront,
+          this.endPointFront == event.origin
+        )
+        // Verificar a origem da mensagem por razões de segurança
+        if (event.origin !== this.endPointFront) {
+          return // Não é a origem esperada, não faça nada
         }
-        // REALIZAR ACOES DE EXECUCAO
-        callback()
-      } else {
-        alert(event.data)
-      }
-  }, false);
+        // Processar a mensagem
+        console.log('Mensagem recebida:', event.data, event.data == 'OK')
+        if (event.data == 'OK') {
+          if (modal) {
+            this.closeModal(modal)
+          }
+          // REALIZAR ACOES DE EXECUCAO
+          callback()
+        } else {
+          alert(event.data)
+        }
+      },
+      false
+    )
   }
-  getTransacoesDocStatus(layout,transacoes, callback = el => true) {
+
+  validacaoFornecedorLote(
+    layout,
+    transacoes,
+    callback = (el) => true,
+    DocAdd = [],
+    options = null
+  ) {
+    console.log(options)
+    let params = {
+      modal: true,
+      target: false,
+    }
+    if (typeof options === 'object') {
+      params = {
+        ...params,
+        ...options,
+      }
+    }
+    console.log(params)
+
+    this.callback = callback
+    const transacoesJoined = transacoes.join(',')
+    const docAddJson = JSON.stringify(DocAdd)
+    const src = `/#/API/${
+      this.sistema
+    }/validacaoFornecedorLote/${layout}/${transacoesJoined}/${this.token}/${btoa(
+      docAddJson
+    )}`
+    let modal = null
+    if (params.modal) {
+      modal = this.addModal(src, params.target)
+    } else {
+      this.addWindow(src)
+    }
+    window.addEventListener(
+      'message',
+      (event) => {
+        console.log(
+          'message FROM',
+          event.origin,
+          this.endPointFront,
+          this.endPointFront == event.origin
+        )
+        // Verificar a origem da mensagem por razões de segurança
+        if (event.origin !== this.endPointFront) {
+          return // Não é a origem esperada, não faça nada
+        }
+        // Processar a mensagem
+        console.log('Mensagem recebida:', event.data, event.data == 'OK')
+        if (event.data == 'OK') {
+          if (modal) {
+            this.closeModal(modal)
+          }
+          // REALIZAR ACOES DE EXECUCAO
+          callback()
+        } else {
+          alert(event.data)
+        }
+      },
+      false
+    )
+  }
+
+  getTransacoesDocStatus(layout, transacoes, callback = (el) => true) {
     // getTransacoesDocStatus(transacoes, callback = el => true) {
     return new Promise((resolve, reject) => {
-      const header = new Headers();
+      const header = new Headers()
       header.append('Content-Type', 'application/json;charset=UTF-8')
       header.append('Access-Control-Allow-Origin', '*')
       header.append('x-access-token', this.token)
       let data = {
         layout: layout,
         sistema: this.sistema,
-        transacoes: transacoes
+        transacoes: transacoes,
       }
       const init = {
         body: JSON.stringify(data),
-        method: "POST",
+        method: 'POST',
         headers: header,
-        mode: "cors",
-        cache: "default",
-      };
-      
+        mode: 'cors',
+        cache: 'default',
+      }
 
-      fetch(this.endPointBack + '/loadRelatorioTransacoesDataAPI' , init)
+      fetch(this.endPointBack + '/loadRelatorioTransacoesDataAPI', init)
         .then(async function (res) {
           const data = await res.json()
           const result = {}
-          data?.transacoes?.forEach(element => {
-            if (!result[element.produtoId]) result[element.produtoId]= {}
-            result[element.produtoId][element.id] = element.nfId ? element.docs : false
+          data?.transacoes?.forEach((element) => {
+            if (!result[element.produtoId]) result[element.produtoId] = {}
+            result[element.produtoId][element.id] = element.nfId
+              ? element.docs
+              : false
           })
           resolve(result)
-        }).catch( e => reject(e))
+        })
+        .catch((e) => reject(e))
     })
   }
   // MODELO DE OBJ : obj = {
@@ -165,16 +249,20 @@ class OfEmbed {
   //   secretaria : number,
   //   sistema : number,
   // }
-  getPreData (obj) {
-    return new Promise ((resolve, reject) => {
-    if (!(obj.cliente &&
-      obj.contrato &&
-      obj.credenciador &&
-      obj.departamento &&
-      obj.fornecedor &&
-      obj.produto &&
-      obj.secretaria &&
-      obj.sistema) ) {
+  getPreData(obj) {
+    return new Promise((resolve, reject) => {
+      if (
+        !(
+          obj.cliente &&
+          obj.contrato &&
+          obj.credenciador &&
+          obj.departamento &&
+          obj.fornecedor &&
+          obj.produto &&
+          obj.secretaria &&
+          obj.sistema
+        )
+      ) {
         console.log(obj)
         throw 'Dados Invalidos'
       }
@@ -186,11 +274,13 @@ class OfEmbed {
       if (obj.transacoes) {
         url += `&transacoes=${obj.transacoes}`
       }
-      fetch(this.endPointBack + url).then(result => {
-        resolve(result.json())
-      }).catch(e => {
-        reject(e)
-      })
+      fetch(this.endPointBack + url)
+        .then((result) => {
+          resolve(result.json())
+        })
+        .catch((e) => {
+          reject(e)
+        })
     })
-  }  
+  }
 }
